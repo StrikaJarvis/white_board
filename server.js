357afa45acf80611357f57bb266d54729fe6e8ba
StrikaJarvis <g.savruhin@yandex.ru>
2026-05-14 13:21:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,47 +9,41 @@ const io = socketIo(server);
 app.use(express.static('public'));
 
 let drawingHistory = [];
-let connectedUsers = {};
+let connectedUsers = {}; // socket.id -> username
 
 io.on('connection', (socket) => {
     console.log('Новый пользователь подключился');
-    
-    // Генерируем случайное имя
-    const username = `User_${Math.floor(Math.random() * 10000)}`;
-    connectedUsers[socket.id] = username;
-    
-    // Отправляем историю рисования новому пользователю
-    socket.emit('loadHistory', drawingHistory);
-    
-    // Сообщаем всем о новом участнике
-    socket.broadcast.emit('userJoined', username);
-    
-    // Обработка событий рисования
+
+    socket.on('setUsername', (username) => {
+        connectedUsers[socket.id] = username;
+        socket.broadcast.emit('userJoined', username);
+        socket.emit('loadHistory', drawingHistory);
+    });
+
     socket.on('draw', (data) => {
         drawingHistory.push(data);
         socket.broadcast.emit('draw', data);
     });
-    
+
     socket.on('fill', (data) => {
         drawingHistory.push(data);
         socket.broadcast.emit('fill', data);
     });
-    
+
     socket.on('clear', () => {
         drawingHistory = [];
         io.emit('clear');
     });
-    
-    // Чат
-    socket.on('chatMessage', (msg) => {
-        const user = connectedUsers[socket.id];
-        io.emit('chatMessage', { username: user, message: msg });
+
+    socket.on('chatMessage', (data) => {
+        // data = { username, message }
+        io.emit('chatMessage', data);
     });
-    
+
     socket.on('disconnect', () => {
-        const user = connectedUsers[socket.id];
-        if (user) {
-            io.emit('userLeft', user);
+        const username = connectedUsers[socket.id];
+        if (username) {
+            io.emit('userLeft', username);
             delete connectedUsers[socket.id];
         }
         console.log('Пользователь отключился');
